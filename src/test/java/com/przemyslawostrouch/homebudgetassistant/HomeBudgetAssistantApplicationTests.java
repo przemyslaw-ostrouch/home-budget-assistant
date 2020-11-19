c5ef08477d3ad7fad003bf88a6598df0df981611
przemyslaw.ostrouch <przemyslaw.ostrouch@jeppesen.com>
2020-11-19 16:38:46 +0100

    initial commit

---
+++ src/test/java/com/przemyslawostrouch/homebudgetassistant/HomeBudgetAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przemyslawostrouch.homebudgetassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBudgetAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
